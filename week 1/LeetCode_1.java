a347159a29d9e1cb01aef8d85c44b2f97e02279f
ryankao78 <36420454+ryankao78@users.noreply.github.com>
2019-04-12 19:45:47 -0400

    Update LeetCode_1.java

--- week 1/LeetCode_1.java
+++ week 1/LeetCode_1.java
@@ -1,1 +1,13 @@
+public static int[] twoSum(int[] nums, int target) {
+    int[] result = new int[]{-1, -1};
 
+    HashMap<Integer, Integer> visited = new HashMap<>();
+    for (int i =0; i < nums.length; i++) {
+        if (visited.containsKey(target - nums[i])) {
+            result[0] = visited.get(target - nums[i]);
+            result[1] = i;
+            return result;
+        }
+        visited.put(nums[i], i);
+    }
+    return result;
