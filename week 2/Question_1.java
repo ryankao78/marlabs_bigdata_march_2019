7a5044695ab77a84501113c9c1e57ae3e80d34a6
ryankao78 <36420454+ryankao78@users.noreply.github.com>
2019-04-12 20:42:53 -0400

    Update Question_1.java

--- week 2/Question_1.java
+++ week 2/Question_1.java
@@ -1,1 +1,43 @@
+import java.io.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.mapreduce.lib.input.*;
+import org.apache.hadoop.mapreduce.lib.output.*;
+import org.apache.hadoop.util.*;
 
+public static class MapClass extends Mapper<LongWritable,Text,Text,Text>
+{
+  public void map(LongWritable key, Text value, Context context)
+  {
+     try{
+        String[] str = value.toString().split("\t", -3);
+        String gender=str[3];
+        context.write(new Text(gender), new Text(value));
+     }
+     catch(Exception e)
+     {
+        System.out.println(e.getMessage());
+     }
+  }
+}
+
+public static class ReduceClass extends Reducer<Text,Text,Text,IntWritable>
+{
+  public int max = -1;
+  public void reduce(Text key, Iterable <Text> values, Context context) throws IOException, InterruptedException
+  {
+     max = -1;
+
+     for (Text val : values)
+     {
+        String [] str = val.toString().split("\t", -3);
+        if(Integer.parseInt(str[4])>max)
+        max=Integer.parseInt(str[4]);
+     }
+
+     context.write(new Text(key), new IntWritable(max));
+  }
+}
